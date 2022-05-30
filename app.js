c361dc6c30c945771138133d00e837b3f6bccb22
Matan Kadosh <kadosh10001@gmail.com>
2022-05-30 12:58:51 +0300

    update gitignore create parse method create config.json update icon

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-function hello(name) {
-  const greeting = `hello ${name}`;
-  return greeting;
+const yaml = require("js-yaml");
+const fs = require("fs");
+
+async function parse(action) {
+  const { filePath } = action.params;
+
+  const doc = yaml.load(fs.readFileSync(filePath, "utf8"));
+
+  return doc;
 }
 
 module.exports = {
-  hello,
+  parse,
 };
