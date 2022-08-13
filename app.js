fa1ad1fde65e2d3b70aa0fee3623b26fc65e4be3
Kenny Marsh <kenny.m@kaholo.io>
2022-08-13 13:39:50 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const yaml = require("js-yaml");
 const fs = require("fs");
 
 async function parse(action) {
   const { filePath } = action.params;
 
-  const doc = yaml.load(fs.readFileSync(filePath, "utf8"));
+  const doc = JSON.parse(fs.readFileSync(filePath, "utf8"));
 
   return doc;
 }
